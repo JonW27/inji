794c4ac7de63cc6a75f5c7af0939a2901ff4748d
JonW27 <18wongj@gmail.com>
2018-02-26 00:52:24 -0500

    no rust compile errors. just need to test

---
+++ draw.rs
@@ -0,0 +1,85 @@
+use display;
+
+pub use matrix::Matrix;
+
+pub fn add_point( points : &mut Matrix, x : i64, y : i64, z : i64){
+    points.m.push(vec![x, y, z]);
+    points.lastcol += 1;
+}
+
+pub fn add_edge( points : &mut Matrix, x0 : i64, y0 : i64, z0 : i64, x1 : i64, y1 : i64, z1 : i64){
+    add_point(points, x0, y0, z0);
+    add_point(points, x1, y1, z1);
+}
+
+pub fn draw_lines( points : Matrix, s : &mut Vec<Vec<[i64; 3]>>, c : [i64; 3]){
+    let m = points.m;
+    let mut pastPoint : Vec<i64> = Vec::new();
+    for i in 0..m.len(){
+        if i % 2 != 0{
+            draw_line(pastPoint[0], pastPoint[1], m[i][0], m[i][1], s, c);
+        }
+        pastPoint = m[i].clone();
+    }
+}
+
+pub fn draw_line( mut x0 : i64, mut y0 : i64, x1 : i64, y1 : i64, s : &mut Vec<Vec<[i64; 3]>>, c : [i64; 3] ){
+    let (mx, my) = (x1 - x0, y1 - y0);
+    if x0 > x1 { // we want the (x1,y1) pair to always be greater so we switch
+        return draw_line(x1, y1, x0, y0, s, c);
+    }
+    let (A, B) = (my, -1 * (mx));
+    let mut d : i64;
+    if my > 0{ // first quadrant
+        if mx > my  { // first octant
+            d = 2*A + B;
+            while x0 <= x1 {
+                display::plot(s, c, x0, y0);
+                if d > 0 {
+                    y0 += 1;
+                    d += 2*B;
+                }
+                x0 += 1;
+                d += 2*A;
+            }
+        }
+        else{ // second octant
+            d = A + 2*B;
+            while y0 <= y1 {
+                display::plot(s, c, x0, y0);
+                if d < 0 {
+                    x0 += 1;
+                    d += 2*A;
+                }
+                y0 += 1;
+                d += 2*B;
+            }
+        }
+    }
+    else { // quadrant 4
+        if mx + my < 0 { // octant 7, mx is always positive so my would need to be less to satisfy
+            d = A - 2*B;
+            while y0 >= y1 {
+                display::plot(s, c, x0, y0);
+                if d > 0 {
+                    x0 += 1;
+                    d += 2*A;
+                }
+                y0 -= 1;
+                d -= 2*B;
+            }
+        }
+        else { // octant 8
+            d = 2*A - B;
+            while x0 <= x1 {
+                display::plot(s, c, x0, y0);
+                if d < 0 {
+                    y0 -= 1;
+                    d -= 2*B;
+                }
+                x0 += 1;
+                d += 2*A;
+            }
+        }
+    }
+}
---
+++ matrix.rs
@@ -0,0 +1,79 @@
+
+pub struct Matrix{
+    pub m : Vec<Vec<i64>>,
+    rows : i64,
+    cols : i64,
+    pub lastcol : i64,
+}
+
+pub fn print_matrix( m : Matrix){
+    // iterators!
+    let m_cols = m.m.iter();
+
+    for m_col in m_cols {
+        let m_rows = m_col.iter();
+        for val in m_rows {
+            println!("{} ", val);
+        }
+        println!("\n");
+    }
+}
+
+// the below method assumes a square matrix
+pub fn ident(m : &mut Matrix){
+    let len = m.m.len();
+
+    for m_col in 0..len {
+        for m_row in 0..len {
+            if m_col == m_row {
+                m.m[m_col][m_row] = 1;
+            }
+            else {
+                m.m[m_col][m_row] = 0;
+            }
+        }
+    }
+}
+
+pub fn matrix_mult(a : Matrix, b : Matrix) -> Matrix{
+
+    let a_refs = a.m;
+    let b_refs = b.m;
+
+    let mut c = new_matrix(a_refs.len() as i64, b_refs.len() as i64);
+
+    for i in 0..a_refs.len(){
+        for j in 0..b_refs[0].len(){
+            let mut sum = 0;
+            for k in 0..a_refs.len() { // usually 3
+                sum += a_refs[i][k] * b_refs[k][j];
+            }
+            c.m[i][j] = sum;
+        }
+    }
+    c
+}
+
+pub fn new_matrix(rows : i64, cols: i64) -> Matrix {
+    let mut tmp : Vec<Vec<i64>> = Vec::new();
+    for y in 0..cols {
+        let tmp_a = y as usize;
+        let mut row : Vec<i64> = Vec::new();
+        tmp.push(row);
+        for _x in 0..rows {
+            &tmp[tmp_a].push(0);
+        }
+    }
+    Matrix {
+        m : tmp,
+        rows,
+        cols,
+        lastcol : 0,
+    }
+}
+
+// no need to free matrix since rust takes care of it with scopes!
+
+// vecs are mutable, so they can grow!
+
+// copying a matrix can be easily done with .clone()!
